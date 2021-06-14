68c119f701ad1aafeeb7e8653bace6d942db3a29
supalapka <yurabalint0103@gmail.com>
2021-06-14 12:43:40 +0300

    Changes for hosting v2

--- utils/notify_admins.py
+++ utils/notify_admins.py
@@ -6,9 +6,9 @@ from data.config import ADMINS
 
 
 async def on_startup_notify(dp: Dispatcher):
-    for admin in ADMINS:
-        try:
-            await dp.bot.send_message(admin, "Бот Запущен")
+    # for admin in ADMINS:
+    # try:
+    await dp.bot.send_message(ADMINS, "Бот Запущен")
 
-        except Exception as err:
-            logging.exception(err)
+    #  except Exception as err:
+    #    logging.exception(err)
